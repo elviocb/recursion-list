b76aad284dc843c06b1ed2d031971f4302876499
Elvio Cavalcante <elvio@madfx.com.br>
2015-06-02 22:47:16 -0700

    Solve flatList's algorithm

--- flatList.js
+++ flatList.js
@@ -2,5 +2,14 @@
 // [[2,7], [8,3], [1,4]] Returns [2,7,8,3,1,4]
 
 function flatList(array){
-    
+    // base case
+    if (array.length === 1) { return array[0] }
+
+    var head = array[0];
+    var tail = array.slice(1);
+
+    var result = flatList(tail);
+
+    return head.concat(result);
+
 }
