23ed142b9b23f270ea54b5d6f92a2bb2e18d646b
Elvio Cavalcante <elvio@madfx.com.br>
2015-06-02 22:54:18 -0700

    Add new toy problem: splitPivotList

---
+++ splitPivotList.js
@@ -0,0 +1,7 @@
+// Split a list of numbers with regard to a pivot element. All elements smaller than the pivot element go in the first return list, all other elements go into the second return list. Both return list are inside one list.
+// [2,7,8,3,1,4] , 4
+// Returns [[2,3,1],[7,8,4]]
+
+function splitPivotList(array) {
+
+}
