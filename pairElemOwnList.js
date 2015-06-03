6489653cb512d6d9e6c8f725accfcb5cb152f602
Elvio Cavalcante <elvio@madfx.com.br>
2015-06-02 22:17:16 -0700

    Add new toy problem: pairElemOwnList

---
+++ pairElemOwnList.js
@@ -0,0 +1,9 @@
+// Put element pairs in their own list. If you have an odd number of elements, add a 0.
+// [2,7,8,3,1,4]
+// Returns [[2,7], [8,3], [1,4]]
+// [3,6,8,1,5]
+// Returns [[3,6],[8,1],[5,0]]
+
+function pairElemOwnList(array) {
+
+}
