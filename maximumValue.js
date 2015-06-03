39127cbb79a66c62d37f77d9f627695fe56cd333
Elvio Cavalcante <elvio@madfx.com.br>
2015-06-02 21:15:58 -0700

    Solve maximumValue's algorithm

--- maximumValue.js
+++ maximumValue.js
@@ -1,3 +1,16 @@
 // Find the maximum value in a list.
 // [2,7,8,3,1,4] Returns 8
 
+function max(array) {
+    // base case
+    if (array.length === 1) { return array[0] }
+
+    // define the head
+    var head = array[0];
+    // define the tail
+    var tail = array.slice(1);
+    // invoke the recursion 
+    var m = max(tail);
+
+    return m > head ? m : head;
+}
