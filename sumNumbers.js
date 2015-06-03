c3b9dea5cd1ea6f1630481e727c3e3cb489d8b12
Elvio Cavalcante <elvio@madfx.com.br>
2015-06-02 21:55:58 -0700

    Solve sumNumbers algorithm

--- sumNumbers.js
+++ sumNumbers.js
@@ -2,5 +2,13 @@
 // [2,7,8,3,1,4] Returns 25
 
 function sumNumbers(array) {
-    
+    // base case 
+    if (array.length === 1) { return array[0] }
+
+    var head = array[0];
+    var tail = array.slice(1);
+
+    var result = sumNumbers(tail);
+
+    return result + head;
 }
