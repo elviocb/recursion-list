faae1357e268f80e95876358e094fb5e86e3f136
Elvio Cavalcante <elvio@madfx.com.br>
2015-06-03 10:06:54 -0700

    Solve evenNumbers algorithm

--- evenNumbers.js
+++ evenNumbers.js
@@ -3,5 +3,16 @@
 // [2,7,8,3,1,4] Returns [2,8,4]
 
 function evenNumbers(array) {
-    
+    // base case
+    if (array.length === 0) { return array}
+
+    var head = array[0];
+    var tail = array.slice(1);
+
+    var result = evenNumbers(tail);
+
+    if(head % 2 === 0) { 
+        return [head].concat(result)
+    }
+    return result;
 }
