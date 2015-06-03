6401c3050eff653ce7cf055fe046c0733569899e
Elvio Cavalcante <elvio@madfx.com.br>
2015-06-02 22:31:22 -0700

    Solve pairElemOwnList's algorithm

--- pairElemOwnList.js
+++ pairElemOwnList.js
@@ -5,5 +5,18 @@
 // Returns [[3,6],[8,1],[5,0]]
 
 function pairElemOwnList(array) {
+    // base case
+    if (array.length === 0) { 
+        return array; 
+    } else if (array.length === 1) {
+        return [array.concat([0])];
+    }
 
+    var head1 = array[0];
+    var head2 = array[1];
+    var tail = array.slice(2);
+
+    var result = pairElemOwnList(tail);
+
+    return [[head1, head2]].concat(result);
 }
