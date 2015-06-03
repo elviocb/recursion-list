cee0b6164647daec6d60024e85a4682595a2bcd0
Elvio Cavalcante <elvio@madfx.com.br>
2015-06-02 22:12:37 -0700

    Solve ownList's algorithm

--- ownList.js
+++ ownList.js
@@ -3,5 +3,15 @@
 // Returns [[2], [7], [8], [3], [1], [4]]
 
 function ownList(array){
+    // base case
+    if (array.length === 1) {return [[array[0]]]}
     
+    var head = array[0];
+    var tail = array.slice(1);
+
+    var recurse = ownList(tail);
+    
+    console.log(recurse);
+    
+    return [[head]].concat(recurse);
 }
