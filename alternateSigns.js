462cc6462cfc1ee02f6813227b1322e60634a05e
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-06-06 09:30:43 -0700

    Add new toy problem: alternateSigns

---
+++ alternateSigns.js
@@ -0,0 +1,10 @@
+// Alternate the numbers sign regardless of their original sign.
+// Start with +;
+// [2,7,8,3,1,4]
+// Returns [2,-7,8,-3,1,-4]
+// [-5,-3,7,1,-6,2]
+// Returns [5,-3,7,-1,6,-2]
+
+function alternateSigns(array){
+  
+};
