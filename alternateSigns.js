f912582fc9f383455d134b06748a92a74ea9e071
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-06-06 10:47:08 -0700

    Solve alternateSign's algorithm

--- alternateSigns.js
+++ alternateSigns.js
@@ -6,5 +6,21 @@
 // Returns [5,-3,7,-1,6,-2]
 
 function alternateSigns(array){
-  
-};
+  // base case
+  if (array.length === 0) { return array }
+
+  var head1 = array[0];
+  var head2 = array[1];
+  var tail = array.slice(2);
+
+  var result = alternateSigns(tail);
+
+  if (head1 < 0) {
+      head1 = -head1
+  } 
+  if (head2 > 0){
+      head2 = -head2
+  }
+
+  return [head1, head2].concat(result);
+}
