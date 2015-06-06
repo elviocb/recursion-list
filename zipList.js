442cbb949c0cc6fcb5f65490a449a9ab63d641bf
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-06-06 10:53:39 -0700

    Add new toy problem: zipList

---
+++ zipList.js
@@ -0,0 +1,6 @@
+// Zip two lists of numbers. If one list is shorter, just append the rest of the other list. First element comes from the first list.
+// [2,7,8,3,1,4], [9,5,1] Returns [2,9,7,5,8,1,3,1,4]
+
+function zipList(list1, list2){
+
+}
