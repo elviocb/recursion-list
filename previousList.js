0db892d60f488f9f3ca66b614e4a61536d9c9422
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-06-08 00:31:34 -0700

    Solve previousList's algorithm

--- previousList.js
+++ previousList.js
@@ -2,5 +2,13 @@
 // [[4,3],[5,6],[8,2]] Returns [4,3,[5,6,[8,2]]]
 
 function previousList(array) {
+  // base case
+  if (array.length === 1) { return array[0]}
 
+  var head = array[0];
+  var tail = array.slice(1);
+
+  var result = previousList(tail);
+
+  return head.concat([result])
 }
