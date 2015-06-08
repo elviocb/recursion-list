100d409e534895ee53506e31b9bb0cde1c6b777f
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-06-08 00:06:23 -0700

    Add new toy problem: augmentElements

---
+++ augmentElements.js
@@ -0,0 +1,7 @@
+// Augument every element in a list with a new value. The elemts are lists themselves.
+// [[],[3],[7]] , 5
+// Returns [[5],[3,5],[7,5]]
+
+function augmentElements(array, val){
+
+}
