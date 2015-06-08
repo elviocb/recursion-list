2305663e29946314b1fa428e8f6d225f28453361
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-06-08 00:49:15 -0700

    Solve sublistValidation's algorithm

--- sublistValidation.js
+++ sublistValidation.js
@@ -1,7 +1,19 @@
 // Return true or false, if a given list starts with a sublist.
 // [2,7,8,3,1,4] , [2,7] Returns true
-// [5,6,7,1,4,6,7,1,3] , [6,7,1] Returns false
+//   Returns false
 
 function sublistValidation (array, valid) {
+  // base case
+  if (valid.length === 0) {
+    return true;
+  } else if (valid[0] !== array[0]) {
+    return false;
+  }
 
+  var tail1 = array.slice(1);
+  var tail2 = valid.slice(1);
+
+  var result = sublistValidation(tail1, tail2);
+  
+  return result;
 }
