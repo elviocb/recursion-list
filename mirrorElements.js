6cb5cb72904d6570d613fca67c4b7048924ed44f
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-06-07 23:59:32 -0700

    Solve mirrorElements algorithm

--- mirrorElements.js
+++ mirrorElements.js
@@ -3,5 +3,12 @@
 // Returns [2,7,8,3,1,4,4,1,3,8,7,2]
 
 function mirrorElements(array) {
+  // base case
+  if (array.length === 0) { return array }
 
+  var head = array[0];
+  var tail = array.slice(1);
+
+  var result = mirrorElements(tail);
+  return [head].concat(result).concat([head]);
 }
