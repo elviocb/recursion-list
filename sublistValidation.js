f48ed6c745e3a99e9ef74f85f2e1de6682663ff7
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-06-08 00:35:18 -0700

    Add new toy problem: sublistValidation

---
+++ sublistValidation.js
@@ -0,0 +1,7 @@
+// Return true or false, if a given list starts with a sublist.
+// [2,7,8,3,1,4] , [2,7] Returns true
+// [5,6,7,1,4,6,7,1,3] , [6,7,1] Returns false
+
+function sublistValidation (array, valid) {
+
+}
