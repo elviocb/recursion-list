b064ca15a56324b29897b5ecbe4f3328abb9586e
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-06-08 00:17:05 -0700

    Solve augmentElements algorithm

--- augmentElements.js
+++ augmentElements.js
@@ -3,5 +3,14 @@
 // Returns [[5],[3,5],[7,5]]
 
 function augmentElements(array, val){
+  // base case
+  if (array.length === 0) { return array }
+
+  var head = array[0];
+  var tail = array.slice(1);
+
+  var result = augmentElements(tail, val);
+
+  return [head.concat([val])].concat(result);
 
 }
