fa6ec72457d5f5f89f3180bb1c9f66a5dee7b8e0
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-06-07 23:05:23 -0700

    Add new toy problem: rangeList

---
+++ rangeList.js
@@ -0,0 +1,10 @@
+// Create a range list counting from a start to an end. Excluding the end.
+// 2,5
+// Returns [2,3,4]
+// 8,1
+// Returns [8,7,6,5,4,3,2]
+// 3,3 Returns []
+
+function rangeList(start, end){
+
+}
