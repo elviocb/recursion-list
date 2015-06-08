3482d6d71e292dc25c9633ed4c0ed09fb06679c7
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-06-07 23:49:05 -0700

    Solve reduceZero's algorithm

--- reduceZeros.js
+++ reduceZeros.js
@@ -2,5 +2,17 @@
 // [2,0,0,0,1,4] Returns [2,0,1,4]
 
 function reduceZeros(array){
+  // base case
+  if (array.length === 0) { return array }
+
+  var head = array[0];
+  var tail = array.slice(1);
+
+  var result = reduceZeros(tail);
   
+  if (result[0] === 0 && head === 0) {
+    return result
+  } else {
+    return [head].concat(result);
+  }
 }
