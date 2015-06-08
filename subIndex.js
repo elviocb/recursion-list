398be14553262cf65c5390a6d6a4094376745c98
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-06-08 01:44:58 -0700

    Solve subIndex's algorithm

--- subIndex.js
+++ subIndex.js
@@ -3,5 +3,16 @@
 // [4,2,7,9,7] , 1, 4 Returns [2,7,9]
 
 function subIndex (array, startIdx, endIdx) {
+  // base case 
+  if (endIdx === 1) { return array[0] }
+
+  var head = array[0];
+  var tail = array.slice(1);
+
+  var result = subIndex(tail, startIdx -1, endIdx -1);
+  
+  var conc = [head].concat(result);
+
+  return (endIdx - startIdx) === conc.length -1 ? result : conc;
   
 }
